73adafee407d21f7d679b774f1006412e5d8dbd4
katsitadzelasha-bit <katsitadzelasha@gmail.com>
2025-11-07 20:05:19 +0400

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,25 @@
+# debugging პითონში გულისხმობს კოდში შეცდომების პოვნას და გამოსწორებას. 
+name=13
+name=14
+print(name)
+
+
+
+
+first_name=("lasha")
+last_name=("katsitadze")
+
+full_name=first_name + " " + last_name
+print(full_name)
+
+
+
+name=("saba")
+name=("giorgi")
+print(name)
+
+
+num = "40"
+print(num * 3)
+
+
